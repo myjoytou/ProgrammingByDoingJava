c673caa14a5739fcbdb709e2fae5089838fddb8d
Vivek Kumar <vivek.kumar@infibeam.net>
2016-08-18 00:02:37 +0530

    Added functionality to handle multiple types of input

--- src/BmiCalculator.java
+++ src/BmiCalculator.java
@@ -5,16 +5,38 @@ import java.util.Scanner;
  * BMI Calculator
  */
 public class BmiCalculator {
-//    private double height;
-//    private double weight;
-//
-//    public BmiCalculator(double height, double weight) {
-//        this.height = height;
-//        this.weight = weight;
-//    }
+    private double height;
+    private double weight;
+    private double inchToMeterConversionRatio = 39.3701;
+    private double poundToKiloConversionRatio = 2.2046;
 
-    private double getBMI(double weight, double height) {
+    private BmiCalculator(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    private void convertToMetricFromImperial(double height, double weight) {
+        this.height = height / inchToMeterConversionRatio;
+        this.weight = weight / poundToKiloConversionRatio;
+    }
+
+    private void convertToMetricFromImperial(int feet, int inch, double weight) {
+        this.height = (feet * 12 + inch) / inchToMeterConversionRatio;
+        this.weight = weight / poundToKiloConversionRatio;
+    }
+
+    private double getBMI() {
         return (weight / (Math.pow(height, 2)));
+    }
+
+    private void getDataFromUser(String heightUnit, String weightUnit) {
+        int feet;
+        int inches;
+        double height;
+        double weight;
+        String info = String.format("Enter you height in %s", heightUnit);
+        System.out.println(info);
+
     }
 
     public static void main(String [] args) {
@@ -25,7 +47,7 @@ public class BmiCalculator {
         height = in.nextDouble();
         System.out.println("Enter you weight in kilograms");
         weight = in.nextDouble();
-        BmiCalculator bc = new BmiCalculator();
-        System.out.println("Your BMI is " + bc.getBMI(weight, height));
+        BmiCalculator bc = new BmiCalculator(weight, height);
+        System.out.println("Your BMI is " + bc.getBMI());
     }
 }
