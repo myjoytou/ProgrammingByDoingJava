01ad344c5243129f9fddc1b0b8cbf6491e5ce597
Vivek Kumar <vivek.kumar@infibeam.net>
2016-08-19 13:02:21 +0530

    BMI Calculator with support for different input type

--- src/BmiCalculator.java
+++ src/BmiCalculator.java
@@ -7,47 +7,68 @@ import java.util.Scanner;
 public class BmiCalculator {
     private double height;
     private double weight;
-    private double inchToMeterConversionRatio = 39.3701;
-    private double poundToKiloConversionRatio = 2.2046;
+    private static final double INCH_TO_METER = 39.3701;
+    private static final double POUND_TO_KILO = 2.2046;
 
-    private BmiCalculator(double height, double weight) {
-        this.height = height;
-        this.weight = weight;
+    Scanner in = new Scanner(System.in);
+
+    private double convertInchesToMeter(double height) {
+        return height / INCH_TO_METER;
     }
 
-    private void convertToMetricFromImperial(double height, double weight) {
-        this.height = height / inchToMeterConversionRatio;
-        this.weight = weight / poundToKiloConversionRatio;
-    }
-
-    private void convertToMetricFromImperial(int feet, int inch, double weight) {
-        this.height = (feet * 12 + inch) / inchToMeterConversionRatio;
-        this.weight = weight / poundToKiloConversionRatio;
+    private double convertPoundsToKilos(double weight) {
+        return weight / POUND_TO_KILO;
     }
 
     private double getBMI() {
         return (weight / (Math.pow(height, 2)));
     }
 
-    private void getDataFromUser(String heightUnit, String weightUnit) {
-        int feet;
-        int inches;
-        double height;
-        double weight;
-        String info = String.format("Enter you height in %s", heightUnit);
-        System.out.println(info);
+    private void printStringMessage(String entity, String entityUnit) {
+        System.out.println("Your " + entity + " in " + entityUnit);
+    }
 
+    private void setDataInMetersAndKilos() {
+        printStringMessage("height", "meters");
+        this.height = in.nextDouble();
+        printStringMessage("weight", "kilos");
+        this.weight = in.nextDouble();
+    }
+
+    private void setDataInInchesAndPounds() {
+        printStringMessage("height", "inches");
+        this.height = convertInchesToMeter(in.nextDouble());
+        printStringMessage("weight", "pounds");
+        this.weight = convertPoundsToKilos(in.nextDouble());
+    }
+
+    private void setDataInFeetInchesAndPound() {
+        printStringMessage("height", "feet");
+        int heightInFeet = in.nextInt();
+        printStringMessage("height", "inches");
+        double heightInInches = in.nextDouble();
+        this.height = convertInchesToMeter(heightInFeet * 12 + heightInInches);
+        printStringMessage("weight", "pound");
+        this.weight = convertPoundsToKilos(in.nextDouble());
     }
 
     public static void main(String [] args) {
-        double height;
-        double weight;
+        BmiCalculator bmiCal = new BmiCalculator();
+        System.out.println("Enter 1 for entering data in meters and kilograms");
+        System.out.println("Enter 2 for entering data in inches and pounds");
+        System.out.println("Enter 3 for entering data in feet, inches and pounds");
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter your height in meters");
-        height = in.nextDouble();
-        System.out.println("Enter you weight in kilograms");
-        weight = in.nextDouble();
-        BmiCalculator bc = new BmiCalculator(weight, height);
-        System.out.println("Your BMI is " + bc.getBMI());
+        int category = in.nextInt();
+        switch (category) {
+            case 1: bmiCal.setDataInMetersAndKilos();
+                break;
+            case 2: bmiCal.setDataInInchesAndPounds();
+                break;
+            case 3: bmiCal.setDataInFeetInchesAndPound();
+        }
+        System.out.println(String.format("%.6f", bmiCal.getBMI()));
     }
+
+
+
 }
